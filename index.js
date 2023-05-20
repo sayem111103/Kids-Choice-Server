99d460f49ae6a9c5b244bf89208c32b50eecb443
sayem111103 <sayemul03@gmail.com>
2023-05-20 10:01:53 +0600

    vercel api getting problem solved

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,10 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.send('Welcome to Kids Choice')
-})
-
+app.get("/", (req, res) => {
+  res.send("Welcome to Kids Choice");
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.glhjkho.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,22 +21,35 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const toyCollection = client.db('ToyDB').collection('allToy');
+    await client.connect((error)=>{
+      if(error){
+        console.log(error);
+        return;
+      }
+    });
+    const toyCollection = client.db("ToyDB").collection("allToy");
 
-    app.get('/allToy', async(req, res)=>{
-        const allToy = await toyCollection.find({}).sort({subCategory: -1}).toArray();
-        res.send(allToy) 
-    })
+    app.get("/allToy", async (req, res) => {
+      const allToy = await toyCollection
+        .find({})
+        .sort({ subCategory: -1 })
+        .toArray();
+      res.send(allToy);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -45,4 +57,4 @@ async function run() {
 }
 run().catch(console.dir);
 
-app.listen(port)
+app.listen(port);
